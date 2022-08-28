9fd3d2a2b00a4925dc749a8f94b658f8968a9e15
Bruno Yu <jackhellowin@gmail.com>
2022-08-28 11:41:09 +0800

    updated

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,29 @@
 $(function () {
   console.log("Hello Bootstrap5");
+  const artistSwiper = new Swiper(".artist__swiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    // centeredSlidesBounds:true,
+    initialSlide: 1,
+    spaceBetween: 0,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 0,
+      // stretch: 0,
+      depth: 200,
+      // modifier: 1,
+      scale: 0.7,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+  // 瀑布流
+  $(".grid").masonry({
+    // options...
+    itemSelector: ".grid-item",
+    columnWidth: 200,
+  });
 });
